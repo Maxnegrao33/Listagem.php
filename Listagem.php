5e5b779866af1f5c70a4e7c2dc8e5455d1d05a4f
Maciel Ribeiro de Oliveira <38005099+Maxnegrao33@users.noreply.github.com>
2023-10-03 20:55:06 -0300

    Update Listagem.php

--- Listagem.php
+++ Listagem.php
@@ -56,5 +56,12 @@ $query_cadastros =mysqli_query($conx, $buscar_cadastros) or die (mysqli_error($c
         </form>
         </td>
     </tr>
+</table>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
 </body>
 
+</html>
